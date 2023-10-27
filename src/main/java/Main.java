78f7146415b7607a14def32167ddef8de6f211c4
Loc <loc@Zipcoders-MacBook-Pro-2.local>
2023-10-27 17:43:47 -0400

    I need to remember to make commits more often :( got the game to work tho

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,42 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.random.RandomGenerator;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
+    public static void main(String[] args) {
+        //random num generator
+        Random rand = new Random();
+        int randomNum = rand.nextInt(1, 10);
 
-    public static void main(String[] args){
-        
+        //just to see and test conditionals
+        System.out.println(randomNum);
+
+        //user input of number 1-10
+        Scanner user = new Scanner(System.in);
+        System.out.println("Guess a number between 1-10");
+        int userNum = user.nextInt();
+        int guessCount = 1;
+
+        //number comparison loop
+        while (true) {
+            if (userNum > randomNum) {
+                System.out.println("Too large");
+                System.out.println("Guess again");
+                userNum = user.nextInt();
+                guessCount++;
+            } else if (userNum < randomNum) {
+                System.out.println("Too small");
+                System.out.println("Guess again");
+                userNum = user.nextInt();
+                guessCount++;
+            } else {
+                System.out.println("Correct Guess");
+                break;
+            }
+        }
+        System.out.println("Number of guesses: " + guessCount);
     }
 }
